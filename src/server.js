0126ff2acfcf00abc4aef235b676693463997b01
Mykyta <angertina777@ukr.net>
2026-06-28 23:06:36 +0300

    add struct for server and create get requests

---
+++ src/server.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: 'Retrieved all notes' });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  res.status(200).json({ message: 'Retrieved note with ID: id_param' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
