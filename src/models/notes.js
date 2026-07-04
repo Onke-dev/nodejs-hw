2f612cd1692ad11dfa6742cab9634c3cd89bf014
Mykyta <angertina777@ukr.net>
2026-07-04 20:07:13 +0300

    add mongoDB and create folder for middleware

---
+++ src/models/notes.js
@@ -0,0 +1,25 @@
+import { Schema } from 'mongoose';
+
+export const titleSchema = new Schema(
+  {
+    title: { type: String, require: true, trim: true },
+    content: { type: String, require: false, trim: true },
+    tag: {
+      type: String,
+      require: false,
+      enum: [
+        'Work',
+        'Personal',
+        'Meeting',
+        'Shopping',
+        'Ideas',
+        'Travel',
+        'Finance',
+        'Health',
+        'Important',
+        'Todo',
+      ],
+    },
+  },
+  { timestamps: true },
+);
