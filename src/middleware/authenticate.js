565749160273e99c1ffbe6df5caf93b45d1a8efa
Mykyta <angertina777@ukr.net>
2026-07-20 11:56:09 +0300

    create logout and middleware auth

---
+++ src/middleware/authenticate.js
@@ -0,0 +1,36 @@
+import createHttpError from 'http-errors';
+import { Session } from '../models/session.js';
+import { User } from '../models/user.js';
+
+export const authenticate = async (req, res, next) => {
+  const { sessionId, accessToken } = req.cookies;
+
+  if (!sessionId || !accessToken) {
+    throw createHttpError(401, 'Session not found');
+  }
+
+  const session = await Session.findOne({
+    _id: sessionId,
+    accessToken,
+  });
+
+  if (!session) {
+    throw createHttpError(401, 'Session not found');
+  }
+
+  const isAccessTokenExpired = session.accessTokenValidUntill < new Date();
+
+  if (isAccessTokenExpired) {
+    throw createHttpError(401, 'Access token expired');
+  }
+
+  const user = await User.findOne(session.userId);
+
+  if (!user) {
+    throw createHttpError(401);
+  }
+
+  req.user = user;
+
+  next();
+};
