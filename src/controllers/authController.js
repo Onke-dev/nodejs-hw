d15f05ef970117e63e66070d77d291bb131e012b
Mykyta <angertina777@ukr.net>
2026-07-27 15:51:32 +0300

    correct authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -64,7 +64,7 @@ export const logoutUser = async (req, res) => {
   res.clearCookie('accessToken');
   res.clearCookie('refreshToken');
 
-  res.status(204).json();
+  res.status(204);
 };
 
 export const refreshUserSession = async (req, res) => {
